d31ee8cb7c42bedf435b696c5502568283899310
knaakotto-tech <knaak.otto@web.de>
2026-08-25 16:33:25 +0200

    ping: kompletter Ablauf ueber MockTransport

--- apps/ping.cpp
+++ apps/ping.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <cstdio>
+#include <robotarm/protocol.hpp>
+#include <robotarm/mock_transport.hpp>
 
 int main() {
-    std::cout << "robotarm ping\n";
+
+    robotarm::MockTransport mock;
+    
+    mock.setzeAntwort({0xFF, 0xFF, 0x01, 0x02, 0x00, 0xFC});
+
+    mock.write(robotarm::build_packet(0x01, 0x01, {}));
+    
+    std::vector<uint8_t> antwort = mock.read(6, 100);
+
+    robotarm::Response r = robotarm::parse_response(antwort);
+
+    printf("%d", r.valid);
+
     return 0;
 }
