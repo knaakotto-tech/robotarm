5af92b36d968328c8a9cb6dbe71221cb68804f34
knaakotto-tech <knaak.otto@web.de>
2026-08-31 14:36:58 +0200

    move_test überarbeitet

--- apps/move_test.cpp
+++ apps/move_test.cpp
@@ -3,6 +3,8 @@
 #include <robotarm/serial_transport.hpp>
 #include <robotarm/registers.hpp>
 #include <cstdio>
+#include <thread>
+#include <chrono>
 
 
 int main() {
@@ -10,7 +12,13 @@ int main() {
     robotarm::SerialTransport serial("/dev/ttyACM0");
     robotarm::ServoBus bus(serial);
 
-    robotarm::Response r = bus.write_register(1, robotarm::R_TORQUE_ENABLE, {1});
-    std::printf("valid=%d error=%02X\n", r.valid, r.error);
+    bus.write_register(1, robotarm::R_TORQUE_ENABLE, {1});
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    bus.write_register(1, robotarm::R_GOAL_POSITION, robotarm::to_low_high(1000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+
+    bus.write_register(1, robotarm::R_GOAL_POSITION, robotarm::to_low_high(3000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
 
 }
