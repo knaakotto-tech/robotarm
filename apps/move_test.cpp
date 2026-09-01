bfd9724adbe7cb1296ccba03382548c4393cba48
knaakotto-tech <236528363+knaakotto-tech@users.noreply.github.com>
2026-09-01 16:17:03 +0200

    test zum auslesen der Motoren

--- apps/move_test.cpp
+++ apps/move_test.cpp
@@ -6,6 +6,24 @@
 #include <thread>
 #include <chrono>
 
+void print_read(const robotarm::Response& r) {
+    
+    if(r.valid == true) {
+        printf("valid=%d station=", r.valid);
+        for(uint8_t b : r.data){
+            printf("%02X", b);
+        }
+    } else{
+        printf("unvollsänding => %d ", r.valid);
+
+        std::printf("RX: %zu Bytes\n", r.data.size()); 
+
+    }
+
+    printf("\n");
+}
+
+
 
 int main() {
 
@@ -15,10 +33,24 @@ int main() {
     bus.write_register(1, robotarm::R_TORQUE_ENABLE, {1});
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
+    robotarm::Response r = bus.read_register(1, robotarm::R_PRESENT_POSITION, 2);
+    
+    print_read(r);
+
+
     bus.write_register(1, robotarm::R_GOAL_POSITION, robotarm::to_low_high(1000));
     std::this_thread::sleep_for(std::chrono::milliseconds(1500));
 
+
+    r = bus.read_register(1, robotarm::R_PRESENT_POSITION, 2);
+
+    print_read(r);
+
     bus.write_register(1, robotarm::R_GOAL_POSITION, robotarm::to_low_high(3000));
     std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+
+
+    r = bus.read_register(1, robotarm::R_PRESENT_POSITION, 2);
+   print_read(r);
 
 }
